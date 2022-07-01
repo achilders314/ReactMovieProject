69c70adf3fcd980c2026f12a791cd995367ce4e2
Alison Childers <alisonchilders314@gmail.com>
2022-07-01 14:06:34 -0500

    Forgot one issue

--- moviebrowser/src/components/Navbar.js
+++ moviebrowser/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({searchText, setSearchText}, {searchState, setSearchState}) => {
   searchState=false;
 
     const updateSearchText = (e) => { 
-      navigate('/react_movie_app/search');
+      navigate('/moviebrowser/search');
       setSearchText(e.target.value);
     }
 
@@ -16,17 +16,17 @@ const Navbar = ({searchText, setSearchText}, {searchState, setSearchState}) => {
 
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-        <a className="navbar-brand" href="/react_movie_app/" onClick="localStorage.clear()">Movie Database</a>
+        <a className="navbar-brand" href="/moviebrowser/" onClick="localStorage.clear()">Movie Database</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
            <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/react_movie_app/">Home</Link>
+            <Link className="nav-link active" aria-current="page" to="/moviebrowser/">Home</Link>
            </li>
            <li className="nav-item">
-            <Link className="nav-link" to="/react_movie_app/about">About</Link>
+            <Link className="nav-link" to="/moviebrowser/about">About</Link>
            </li>
            <li className="nav-item">
             <Link className="nav-link disabled" to="/" tabIndex="-1" aria-disabled="true">Coming Soon</Link>
